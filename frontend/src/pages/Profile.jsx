4fa1eb1233761b335d8404ec896621b8f8f42b5a
HariRai123 <harirai4132@gmail.com>
2026-08-27 17:06:53 +0530

    fix: import tryonservice

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,7 +16,7 @@ import {
 
 import { useAuth } from "../context/AuthContext";
 import { getProfile } from "../services/authService";
-import { getMyTryOnResults } from "../services/tryOnService";
+import { getMyTryOnResults } from "../services/tryOnService.js"
 
 function Profile() {
   const { user, updateUserProfile } = useAuth();
